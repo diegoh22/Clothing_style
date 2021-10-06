264bf544a50e4d7ae2de670a16e3bf0c7ad488bf
diegoh22 <diegohofficial@gmail.com>
2021-10-06 15:29:04 +0000

    add user loop request to enter the valid number and correctnumbers

--- run.py
+++ run.py
@@ -13,8 +13,40 @@ SCOPE_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPE_CREDS)
 SHEET = GSPREAD_CLIENT.open('Clothing-Style')
 
-sales = SHEET.worksheet('sales')
 
-data = sales.get_all_values()
+def get_sales_numbers():
 
-print(data)
+    """
+    Get the sales numbers of the day
+    """
+    while True:
+        print('Please enter de sales numbers of the day')
+        print('Enter the numbers separate by a commas')
+        print(' six numbers, Explate 10,50,100,65,48,98,\n')
+
+        numbers_str = input("Enter the numbers here: ")
+
+        sales_numbers = numbers_str.split(",")
+
+        if validate_data(sales_numbers):
+            print("Data is valid!")
+            break
+    return sales_numbers
+
+
+def validate_data(values):
+
+    try:
+        [int(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(
+                f"Enter six numbers, you enter:\n {len(values)}"
+            )
+    except ValueError as e:
+        print(f"invalid date: {e}, please try again.\n")
+        return False
+
+    return True
+
+
+data = get_sales_numbers()
